f6b74afc61b33d867dfeea82dd03e7cf7df2feea
Brad John <dom22002@byui.edu>
2024-01-19 20:08:26 -0700

    I added a code system so no one else can read the journal.

--- prove/Develop02/Saver.cs
+++ prove/Develop02/Saver.cs
@@ -1,13 +1,21 @@
 using System;
 using System.IO;
+using System.Text;
 class FileWriter
 {
     public static void WriteFile(string path, string content)
     {
     using (StreamWriter writer = new StreamWriter(path, true))
         {
-        writer.WriteLine(content);
+        string code = StringToBinary(content);
+        writer.WriteLine(code);
         }
     Console.WriteLine($"Content has been written to {path}");
+    }
+    // This converts the message to binary so no on else can read it
+    static string StringToBinary(string input)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes(input);
+        return string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
     }
 }
