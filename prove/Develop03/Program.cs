463c94d165cc8d9c115e5a4dd2d6567a2d392e84
Brad John <dom22002@byui.edu>
2024-01-25 11:20:24 -0700

    Scripture assignment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,6 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // I brought back the menu for more options, like writing your own verses or loading one from a file.
+        // The default scripture file is just the instructions, which I used to test for bigger scriptures.
         Menu.Initialize();
     }
 }
