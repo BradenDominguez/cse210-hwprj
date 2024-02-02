eb63df87ffda4bde120ce39b8430efca1cafc9d3
Brad John <dom22002@byui.edu>
2024-02-01 18:14:22 -0700

    Assignment assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Test", "Mystery");
+        Console.WriteLine(assignment.GetSummary());
+        
+        MathAssignment mathassignment = new MathAssignment("Lynda", "Trigonomonotry", "A", "1-13");
+        Console.WriteLine(mathassignment.GetSummary());
+        Console.WriteLine(mathassignment.GetHomeworkList());
+
+        WritingAssignment writingassignment = new WritingAssignment("Jason", "Briterature", "Great Gatsby");
+        Console.WriteLine(writingassignment.GetSummary());
+        Console.WriteLine(writingassignment.GetWritingInfo());
     }
 }
